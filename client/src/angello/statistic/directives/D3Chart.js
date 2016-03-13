ad3f60ec6be21d7fb7513295985a7a2cfbb30744
DMinerJackie <zjhxp_1990@qq.com>
2016-03-13 20:14:21 +0800

    bug hunting: 1.solve recreate d3chart bug 2.not approprite show d3chart in other module such as switch to dashboard 3.move svg under div not in same level with div

--- client/src/angello/statistic/directives/D3Chart.js
+++ client/src/angello/statistic/directives/D3Chart.js
@@ -1,52 +1,52 @@
-angular.module("Angello.Statistic")
-.directive("d3chart",function(){
-                return{
-                 restrict:'EA',
-                 scope: {
-                	stories: "=",
-                	statusArr: "="
-            	},
-                link: function($scope, $element, $attrs){//link or controller
-            		
-                	var width = 400;
-    		 		var height = 400;
-    		 		var dataset = $scope.statusArr;
-    		 		var svg = d3.select("body")
-    		 					.attr("align","center")// align svg to center
-    		 					.append("svg")
-    		 					.attr("width", width)
-    		 					.attr("height", height);
-    		 		var pie = d3.layout.pie();
-    		 		var piedata = pie(dataset);	
-    		 		var outerRadius = 150;	//外半径
-    		 		var innerRadius = 0;	//内半径，为0则中间没有空白
-    		 		var arc = d3.svg.arc()	//弧生成器
-    		 					.innerRadius(innerRadius)	//设置内半径
-    		 					.outerRadius(outerRadius);	//设置外半径	
-    		 		var color = d3.scale.category10();	
-    		 		var arcs = svg.selectAll("g")
-    		 					  .data(piedata)
-    		 					  .enter()
-    		 					  .append("g")
-    		 					  .attr("transform","translate("+ (width/2) +","+ (width/2) +")");				  
-    		 		arcs.append("path")
-    		 			.attr("fill",function(d,i){
-    		 				return color(i);
-    		 			})
-    		 			.attr("d",function(d){
-    		 				return arc(d);
-    		 			});		
-    		 		arcs.append("text")
-    		 			.attr("transform",function(d){
-    		 				return "translate(" + arc.centroid(d) + ")";
-    		 			})
-    		 			.attr("text-anchor","middle")
-    		 			.text(function(d){
-    		 				return d.data;
-    		 			});
-    		 		console.log(dataset);
-    		 		console.log(piedata);
-                    }
-            	}
-		 		
-            });
+angular.module("Angello.Statistic").directive(
+		"d3chart",
+		function() {
+			return {
+				restrict : 'EA',
+				scope : {
+					statusArr : "=",
+					existed: "="
+				},
+				link : function($scope, $element, $attrs) {// link or controller
+						var width = 400;
+						var height = 400;
+						var dataset = $scope.statusArr;
+						if($("svg").length >= 1){  //if exist svg in html source code return, else create 
+							return;
+						}else{
+//							d3.select("body").attr("align", "center");
+							var svg = d3.select(".flag").attr("align", "center")// align svg to center and move svg into div
+							.append("svg").attr("width", width).attr("height", height);
+							var pie = d3.layout.pie();
+							var piedata = pie(dataset);
+							var outerRadius = 150; // 外半径
+							var innerRadius = 0; // 内半径，为0则中间没有空白
+							var arc = d3.svg.arc() // 弧生成器
+							.innerRadius(innerRadius) // 设置内半径
+							.outerRadius(outerRadius); // 设置外半径
+							var color = d3.scale.category10();
+							var arcs = svg.selectAll("g").data(piedata).enter()
+									.append("g").attr(
+											"transform",
+											"translate(" + (width / 2) + ","
+													+ (width / 2) + ")");
+							arcs.append("path").attr("fill", function(d, i) {
+								return color(i);
+							}).attr("d", function(d) {
+								return arc(d);
+							});
+							arcs.append("text").attr("transform", function(d) {
+								return "translate(" + arc.centroid(d) + ")";
+							}).attr("text-anchor", "middle").text(function(d) {
+								return d.data;
+							});
+						}
+						
+					}
+				
+//				controller: function($scope){
+//						$scope.myUser.existed = "false";
+//					}
+			}
+			$scope.existed = false;
+		});
