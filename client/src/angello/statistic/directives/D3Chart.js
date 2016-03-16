941ca0ab6801f8b3e442591b370f4e8fc592ee3a
DMinerJackie <zjhxp_1990@qq.com>
2016-03-16 22:29:29 +0800

    add tooltip on d3chart

--- client/src/angello/statistic/directives/D3Chart.js
+++ client/src/angello/statistic/directives/D3Chart.js
@@ -42,8 +42,64 @@ angular.module("Angello.Statistic").directive(
 							arcs.append("text").attr("transform", function(d) {
 								return "translate(" + arc.centroid(d) + ")";
 							}).attr("text-anchor", "middle").text(function(d) {
+								console.log(d);
 								return d.data[0];
 							});
+							
+							
+							arcs.append("title")
+				    		.text(function(d)
+				    		{
+				    			return d.data[0]+" : "+d.data[1];
+				    		});
+				    		
+				    	
+				    	//------------------------------------2.div提示框,通过设置透明度（opacity属性）实现 显示和隐藏	
+				    		//添加提示框的div
+				    		var tooltip = d3.select("body").append("div")
+//				    					.attr("class","tooltip") //用于css设置类样式
+				    					.attr("opacity",0.0);
+				    		
+				    		//响应事件
+				    			//-鼠标移入事件
+				    		arcs.on("mouseover",function(d)
+				    		{	
+				    			//设置tooltip文字
+				    			tooltip.html(d.data[0]+" : "+d.data[1])
+				    			//设置tooltip的位置(left,top 相对于页面的距离) 
+				    					.style("left",(d3.event.pageX)+"px")
+				    					.style("top",(d3.event.pageY+20)+"px")
+				    					.style("opacity",1.0);
+				    		})
+				    		//--鼠标移出事件
+				    		.on("mouseout",function(d)
+				    		{
+				    			tooltip.style("opacity",0.0);
+				    		}); 
+				    		
+				    		//-----------------------------------3.svg中的text标签提示框
+				    	 	arcs.on("mouseover",function(d)
+				    		{
+				    			var x =d3.event.pageX;
+				    			var y =d3.event.pageY+30;
+								//添加标签
+				    			svg.append("text")
+				    				.attr("id","tooltip")    				
+				    				.attr("x",x)
+				    				.attr("y",y) 
+				    				.attr("text-anchor","middle")  
+									.attr("font-family","sans-setif")  
+									.attr("font-size","11px")  
+									.attr("font-weight","bold")  
+									.attr("fill","black")  
+									//文本内容
+				    				.text("item number:"+d.value); 							
+				    		})
+				    		//鼠标移出时通过ID移除标签
+				    	 	.on("mouseout",function(d)
+				    		{
+				    			d3.select("#tooltip").remove();
+				    		});  
 						}
 						
 					}
